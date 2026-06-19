7fd2212a7f42ee0bba22a0b60bed1df8486cba00
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2026-06-19 22:11:52 +0200

    🐛 [Bug Fix] show switch only on login

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2557768508" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1919961682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
