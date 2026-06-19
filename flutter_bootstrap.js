1b7c393d92bed6cfad442464330d24dcba3a19b5
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2026-06-19 20:21:20 +0200

    ✨ [New Feature] switch between envs & see users summary

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3121919942" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2017590756" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
