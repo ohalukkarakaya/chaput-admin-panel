3307e02f86e13c139ad741d62719b96d4aae2790
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2026-06-20 15:49:57 +0200

    ✨ [New Feature] login exception

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1919961682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3113385205" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
