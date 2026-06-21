0937ed3a310b22c341380726f600a46b06e964c8
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2026-06-21 15:10:17 +0200

    ✨ [New Feature] User feedback

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3113385205" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "914698909" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
