a69354205e5ac1192d08b69307daaffe93068a75
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2026-07-01 19:07:25 +0200

    ✨ [New Feature] analytics support

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1956623221" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4248515015" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
