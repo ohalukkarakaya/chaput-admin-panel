636a909ff0ffc59a8225fbb488caaedbaa548971
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2026-08-23 02:36:02 +0300

    🐛 [Bug Fix] shadow ban

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3442727184" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3744341052" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
